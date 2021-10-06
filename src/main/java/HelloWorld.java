8d0b3ca2db04c2d5ecb2d709dc4e9b3c932d2226
MohsinKhan7 <mohsinkhan.mmk@gmail.com>
2021-10-06 21:55:01 +0500

    draft

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Mohsin Khan on 9/30/21.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] argsV)
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/trainings/springboot/TestApplication.java
@@ -0,0 +1,18 @@
+package com.trainings.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mohsin Khan on 10/6/21.
+ */
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
+
+
